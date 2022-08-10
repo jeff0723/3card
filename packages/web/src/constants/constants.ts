e4505ebd338f6751e0e5d55075b6eae5247b2545
jeffreylin0723 <qwertzxc1478963@gmail.com>
2022-08-10 16:37:00 +0800

    change env

--- packages/web/src/constants/constants.ts
+++ packages/web/src/constants/constants.ts
@@ -1,10 +1,10 @@
 export const ALCHEMY_KEY = process.env.NEXT_PUBLIC_ALCHEMY_KEY
 
-export const API_URL = process.env.NODE_ENV === 'production'
+export const API_URL = process.env.NEXT_PUBLIC_ENV === 'production'
     ? 'https://api.lens.dev'
     : 'https://api-mumbai.lens.dev'
 
-export const IS_MAINNET = process.env.NODE_ENV === 'production' ? true : false
+export const IS_MAINNET = process.env.NEXT_PUBLIC_ENV === 'production' ? true : false
 
 export const OPENSEA_URL = IS_MAINNET ? 'https://opensea.io' : 'https://testnets.opensea.io'
 type ChainIdMap = { [chainId: number]: string }
